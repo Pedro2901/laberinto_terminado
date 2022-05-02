c6faea52b9fcf44aee0f4ffd566ae4ff1c939f61
Pedro2901 <ppjimenez@uninorte.edu.co>
2022-05-02 01:46:39 -0400

    asd
    asd

--- Assets/Scripts/moleMovement.cs
+++ Assets/Scripts/moleMovement.cs
@@ -2,10 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Direction
-{
-    up,down,left,right
-}
 public class moleMovement : MonoBehaviour
 {
 
